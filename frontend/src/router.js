57b10eaa5276c3251fe7a754a5b9fe4a9fd5deb7
Eliza Ahmed <elizarafee@gmail.com>
2020-12-17 21:12:07 +0000

    welcome view created

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+//import Home from "./views/Home.vue";
+import Welcome from "./views/Welcome.vue";
 
 Vue.use(Router);
 
@@ -10,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "welcome",
+      component: Welcome
     },
     {
       path: "/about",
---
+++ frontend/src/views/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Hello! Everyone.</h1>
+        <p class="lead text-muted">
+          Welcome to my Book store.<br />
+          I am Eliza Ahmed, graduated from Metropolitan University with B.Sc.
+          Computer Science & Engineering last February. Since I have passed, I
+          wanted to sell some of my used books.
+        </p>
+        <p>
+          <router-link to="/books" class="btn btn-outline-success btn-lg my-2"
+            >Have a look on my Books</router-link
+          >
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
