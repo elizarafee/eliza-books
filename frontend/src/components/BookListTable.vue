bec2045ee74148c607cf5bba2f269d1215118edc
Eliza Ahmed <elizarafee@gmail.com>
2020-12-18 16:35:26 +0000

    dashboard list updated

--- frontend/src/components/BookListTable.vue
+++ frontend/src/components/BookListTable.vue
@@ -1,64 +1,66 @@
 <template>
-    <div class="py-5">
-      <div class="container">
-        <div class="pb-5">
-          <h3 class="float-start text-left">List of Books</h3>
-          <router-link
-            to="/books/create"
-            class="btn btn-outline-secondary btn-sm float-end"
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-plus-circle"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-              />
-            </svg>
-            Add new book</router-link
+  <div class="py-5">
+    <div class="container">
+      <div class="pb-5">
+        <h3 class="float-start text-left">List of Books</h3>
+        <router-link
+          to="/books/create"
+          class="btn btn-outline-secondary btn-sm float-end"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-plus-circle"
+            viewBox="0 0 16 16"
           >
-        </div>
-
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col" width="5%">#</th>
-              <th scope="col" width="40%" class="text-start">Title</th>
-              <th scope="col" width="20%" class="d-none d-sm-table-cell">
-                Purchased At
-              </th>
-              <th scope="col" width="">Handle</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td class="text-start">Mark <span class="badge bg-warning text-dark">Sold</span></td>
-              <td class="d-none d-sm-table-cell">Otto</td>
-              <td>
-
-<book-mark-as-sold />
-<remove-book />
-
-              </td>
-            </tr>
-          </tbody>
-        </table>
+            <path
+              fill-rule="evenodd"
+              d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+            />
+          </svg>
+          Add new book</router-link
+        >
       </div>
+
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col" width="5%">#</th>
+            <th scope="col" width="40%" class="text-start">Title</th>
+            <th scope="col" width="20%" class="d-none d-sm-table-cell">
+              Selling Price
+            </th>
+            <th scope="col" width="">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="book in books" :key="book.id">
+            <th scope="row">{{ book.id }}</th>
+            <td class="text-start">
+              <router-link class="px-1" :to="`/books/${book.id}`">
+                {{ book.title }}
+              </router-link>
+              <span class="badge bg-warning text-dark">Sold</span>
+            </td>
+            <td class="d-none d-sm-table-cell">&#163;{{ book.selling_price }}</td>
+            <td>
+              <book-mark-as-sold :book="book" />
+              <remove-book :book="book" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <script>
-//import axios from "axios";
-
+import axios from "axios";
 import BookMarkAsSold from "@/components/BookMarkAsSold";
 import RemoveBook from "@/components/RemoveBook";
 
@@ -68,25 +70,21 @@ export default {
     BookMarkAsSold,
     RemoveBook,
   },
+
   data() {
     return {
-      serverMessage: null,
-      isModalVisible: false,
+      books: {},
     };
   },
   async created() {
-    console.log("created");
-    // const response = await axios.get("/api/start");
-    //  this.serverMessage = "All Books";
-  },
-   methods: {
-      showModal() {
-        this.isModalVisible = true;
-      },
-      closeModal() {
-        this.isModalVisible = false;
-      }
+    const response = await axios.get("/api/books");
+    if (response.data.status == "success") {
+      this.books = response.data.data;
+    } else if (response.data.status == "error") {
+      this.$toast.error(response.data.message, {
+        duration: 10000,
+      });
     }
+  },
 };
 </script>
-
